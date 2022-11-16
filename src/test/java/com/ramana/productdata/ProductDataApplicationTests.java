7ec3ef5cde0915a077306911a0b323ea8e379df9
ramanaiahpade786 <101059575+ramanaiahpade786@users.noreply.github.com>
2022-11-16 19:17:28 +0530

    Initial commit

---
+++ src/test/java/com/ramana/productdata/ProductDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramana.productdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
